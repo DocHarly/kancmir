233ed7a0408584c359a7c9be17281d613fdb6651
Alexandr Treboganov <alex@quokka.pro>
2017-09-02 16:59:12 +0300

    cart

--- app/js/common.js
+++ app/js/common.js
@@ -7,6 +7,29 @@ $(function() {
 		smartSpeed: 700
 	});
 
+	$('.cart-prices').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		dots: false,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		nav: true,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			480: {
+				items: 2
+			},
+			800: {
+				items: 3
+			},
+			1100: {
+				items: 4
+			}
+		}
+	});
+
 	//Resize Window
 	function onResize() {
 		$('.foto').equalHeights();
