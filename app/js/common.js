5761e0218a881db66d1be658a8ff7a13b911967d
king130594 <web@quokka.pro>
2017-09-12 20:49:23 +0300

    lo

--- app/js/common.js
+++ app/js/common.js
@@ -114,8 +114,6 @@ $(document).ready(function() {
 			return false;
 		});
 	});
-<<<<<<< HEAD
-=======
 
 jQuery("#slider").slider({
 	min: 0,
@@ -159,4 +157,17 @@ $(document).ready(function() {
 	});
 });
 
->>>>>>> 02bc322aeff1c2e18769d830ffc233f365b51f3c
+(function($) {
+$(function() {
+
+  $('ul.tabs__caption').each(function() {
+    $(this).find('li').each(function(i) {
+      $(this).click(function(){
+        $(this).addClass('active').siblings().removeClass('active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq(i).addClass('active');
+      });
+    });
+  });
+
+})
+})(jQuery)
